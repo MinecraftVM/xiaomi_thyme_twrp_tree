6cb4d361dc78877a6f9c8f6334da36d847374f5a
MCVM <129513410+MinecraftVM@users.noreply.github.com>
2025-01-05 07:43:41 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.kona \
+    bootctrl.kona.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
@@ -26,6 +29,7 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libgptutils \
     libz \
     libcutils
+    bootctrl
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
